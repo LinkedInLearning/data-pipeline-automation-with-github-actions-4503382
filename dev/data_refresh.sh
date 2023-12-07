4887cea04016113a4463b571332df4d9518ae36e
RamiKrispin <ramkrisp@umich.edu>
2023-12-07 00:09:14 -0800

    test the refresh on gh

--- dev/data_refresh.sh
+++ dev/data_refresh.sh
@@ -3,18 +3,14 @@
 Rscript -e "source('./dev/04_data_refresh.R')"
 
 echo "Finish"
-
-git config --global --add safe.directory /__w/data-pipeline-automation-with-github-actions-4503382/data-pipeline-automation-with-github-actions-4503382
+p=$(pwd)
+git config --global --add safe.directory $p
 
 if [[ "$(git status --porcelain)" != "" ]]; then
-    echo "Set user name"
     git config --global user.name 'RamiKrispin'
-    echo "Set user email"
     git config --global user.email 'ramkrisp@umich.edu'
-    echo "Add new files"
     git add csv/*
     git add metadata/*
-    echo "Commit"
     git commit -m "Auto update of the data"
     git push origin stg
 else
