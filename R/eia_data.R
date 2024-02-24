372382cc71df3c8a0953e031084f3d0e32237103
RamiKrispin <ramkrisp@umich.edu>
2024-02-23 18:20:41 -0800

    update the R functions/

--- R/eia_data.R
+++ R/eia_data.R
@@ -26,6 +26,17 @@ metadata_tamplate <- function() {
 }
 
 
+#' Create Metadata Table
+#' @description The function creates metadata table to a given inputs
+#' @param data the input data
+#' @param start the start argument of the data request,
+#' will be use to evluate if the first timestamp of the series is aligned with the request
+#' @param end the end argument of the data request,
+#' will be use to evluate if the last timestamp of the series is aligned with the request
+#' @param type the refresh type, either "backfill" or "refresh"
+#' @return A data.frame object with the data input metadata
+
+
 create_metadata <- function(data, start, end, type) {
     meta <- metadata_tamplate()
     if (!is.null(data)) {
@@ -57,4 +68,58 @@ create_metadata <- function(data, start, end, type) {
     } else {
         meta$comments <- paste(meta$comments, "No new data is available; ", sep = "")
     }
+
+    return(meta)
+}
+
+
+create_metadata <- function(data, start, end, type) {
+    meta <- list(
+        index = NULL,
+        parent = NULL,
+        subba = NULL,
+        time = Sys.time(),
+        start = start,
+        end = end,
+        start_act = NULL,
+        end_act = NULL,
+        start_match = NULL,
+        end_match = NULL,
+        n_obs = NULL,
+        na = NULL,
+        type = type,
+        update = FALSE,
+        success = FALSE,
+        comments = ""
+    )
+    if (!is.null(data)) {
+        meta["parent"] <- unique(data$parent)
+        meta["subba"] <- unique(data$subba)
+        meta["start_act"] <- min(data$period)
+        meta["end_act"] <- max(data$period)
+        meta["start_match"] <- ifelse(min(data$period) == start, TRUE, FALSE)
+        meta["end_match"] <- ifelse(max(data$period) == end, TRUE, FALSE)
+        meta["n_obs"] <- nrow(data)
+        meta["na"] <- sum(is.na(data$value))
+        if (meta["start_match"] && meta["end_match"] && meta["type"] == "refresh" && meta["na"] == 0) {
+            meta["success"] <- TRUE
+        } else {
+            meta["success"] <- FALSE
+        }
+
+        if (!meta["start_match"]) {
+            meta["comments"] <- paste(meta["comments"], "The start argument does not match the actual; ", sep = "")
+        }
+        if (!meta["end_match"]) {
+            meta["comments"] <- paste(meta["comments"], "The end argument does not match the actual; ", sep = "")
+        }
+
+        if (meta["na"] != 0) {
+            meta["comments"] <- paste(meta["comments"], "Missing values were found; ", sep = "")
+        }
+    } else {
+        meta["comments"] <- paste(meta["comments"], "No new data is available; ", sep = "")
+    }
+
+    return(meta)
 }
